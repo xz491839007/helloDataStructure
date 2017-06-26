27523d1425a157bd9b2c0da113cfb2b46a9e7d7b
xz491893007 <491839007@qq.com>
2017-06-26 12:37:01 +0800

    初始化项目

---
+++ helloDataStructure/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  helloDataStructure
+//
+//  Created by mac on 2017/6/26.
+//  Copyright © 2017年 com.xz. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+
+int main(int argc, const char * argv[]) {
+
+
+    printf("Hello, World!\n");
+
+    
+    return 0;
+}
